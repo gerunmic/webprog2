85f2f0551e8c60ef9494a9ee16b420ec6da877bf
gerunmic <gerunmic@students.zhaw.ch>
2013-03-19 15:23:41 +0100

    Validate length of Description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	validates_presence_of :title, :description, :price
 	validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.'}
+	validates_length_of  :description, :minimum => 20
 end
