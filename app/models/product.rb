265af6a2d539e6e308cba928aeaa8e61da69292a
gerunmic <gerunmic@students.zhaw.ch>
2013-03-19 15:16:22 +0100

    Erste Validierung für Bilder

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
 	validates_presence_of :title, :description, :price
+	validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.'}
 end
