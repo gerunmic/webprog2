4eafe35dc8f1c834b6e7a0d073472ebba1106e1e
gerunmic <gerunmic@students.zhaw.ch>
2013-04-09 15:13:01 +0200

    'cart_creation_chapter'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,20 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+	
+	before_destroy :ensure_note_referenced_by_any_line_item
+	
 	validates_presence_of :title, :description, :price
 	validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.'}
 	validates_length_of  :description, :minimum => 20
+	
+	private
+		
+		def ensure_note_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
 end
